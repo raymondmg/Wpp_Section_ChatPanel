43dbb9d40d5875ada5cfe48de7303fc507250c5f
RaymondMcGuire <raymondmg@foxmail.com>
2015-08-12 19:20:06 +0800

    0812Change

--- Wpp_Section_ChatPanel/js/ChatPanel.js
+++ Wpp_Section_ChatPanel/js/ChatPanel.js
@@ -1,4 +1,94 @@
 
+/********************
+**ChatPanel初始化启动
+*********************/
+//初始化状态
+function initState() {
+    //TODO显示当前日期
+    $("#_spanDate").text("星期" + new Date().getDay().toString());
+    //设定初始化时处于用户聊天面板
+    changeSelectedState("chat");
+}
+
+/********************************
+**SignalR  浏览器客户端连接服务器
+*********************************/
+
+//浏览器连接服务器
+function ConnectToServer() {
+    //用户输入登录名
+    while (userID.length == 0) {
+        userID = window.prompt("Test:请输入登录名称");
+    }
+
+    //与服务器进行连接
+    chat = $.connection.chatHub;
+
+    //连接打开
+    $.connection.hub.start().done(function () {
+        //连接完成，将登陆用户的信息传输给服务器
+        chat.server.userConnected(userID);
+    });
+
+    //取得所有聊天信息内容
+    chat.client.getList = function (userTime, userList) {
+        //显示当前对应的时间段内容,不需要传送告知服务器
+        addTimeLabel(userTime, false);
+        //将当前时间段内对应的内容显示
+        $.each(userList, function (index, data) {
+            UserAcceptMsg("img/2.jpg", data.message);
+        });
+    }
+
+    //显示初始化的聊天信息未读数目
+    chat.client.initUnReadMsg = function (unReadCount) {
+        updateUnReadMsgCount(unReadCount);
+    }
+}
+
+/**************************************
+**SignalR  客户端与服务器信息传递与交互
+***************************************/
+
+//当前用户发送信息
+function UserSendMsg(imgSrc, sendInfo) {
+    //向服务器发送请求，检测自己是否可以发送信息
+    chat.server.judgeCanMeSendMsg(imgSrc.toString(), sendInfo.toString());
+}
+
+//服务器告知客户端是否可以发送信息(以及获取是否含有时间)
+function UserCanSendMsg(canSend, havePreTime, imgSrc, sendInfo) {
+    if (canSend) {
+        //可以
+        if (havePreTime) {
+            ChatPanelAddPreTimeLabel();
+        }
+        //添加自己的信息到面板上
+        var data = new UserChatData();
+        data.bind(imgSrc, sendInfo);
+        loadChatInfo(data, true);
+        //更新面板显示
+        updateScrollTop();
+        //服务器广播所有人发送的信息
+        chat.server.sendAllMessage(result);
+    }
+    else {
+        return;
+    }
+}
+
+//用户接收到他人发送的信息
+function UserAcceptMsg(imgSrc, sendInfo) {
+    var data = new UserChatData();
+    data.bind(imgSrc, sendInfo);
+    loadChatInfo(data, false);
+    //更新面板显示
+    updateScrollTop();
+
+    //统计量显示
+    //若用户当前未打开右侧面板  ，统计量增加
+    UnReadMsgHandler();
+}
 
 /*********
 **边栏显示
@@ -519,9 +609,98 @@ var slidebars = function () {
 	window.onresize = this.css.bind( this );
 };
 
+/**********************
+*用户聊天数据类型和绑定
+***********************/
+//用户聊天数据类型
+function UserChatData() {
+    this.imgSrc;
+    this.userInfo;
+}
+//用户聊天数据绑定
+UserChatData.prototype.bind = function (img, info) {
+    this.imgSrc = img;
+    this.userInfo = info;
+}
 /************
 *显示聊天信息
 ************/
+
+//切换到用户选择状态
+function changeSelectedState(state) {
+    switch (state) {
+        case "chat":
+            currentState = "chat";
+            FriendNormal();
+            ChatActive();
+            break;
+        case "friend":
+            currentState = "friend";
+            ChatNormal();
+            FriendActive();
+            break;
+    }
+}
+
+//聊天面板隐藏
+function ChatPanelHide() {
+
+}
+//聊天面板显示
+function ChatPanelShow() {
+    //添加animate类使得聊天面板可见
+    //$("chat-messages")
+}
+//朋友面板隐藏
+function FriendPanelHide() {
+
+}
+//朋友面板显示
+function FriendPanelShow() {
+
+}
+
+//聊天选项激活
+function ChatActive() {
+    if ($("#topmenu_chat").hasClass("chats")) {
+        $("#topmenu_chat").removeClass("chats");
+    }
+    if (!$("#topmenu_chat").hasClass("chatsActive")) {
+        $("#topmenu_chat").addClass("chatsActive");
+    }
+
+
+}
+
+//聊天选项正常显示
+function ChatNormal() {
+    if ($("#topmenu_chat").hasClass("chatsActive")) {
+        $("#topmenu_chat").removeClass("chatsActive");
+    }
+    if (!$("#topmenu_chat").hasClass("chats")) {
+        $("#topmenu_chat").addClass("chats");
+    }
+}
+
+//好友选项激活
+function FriendActive() {
+    if ($("#topmenu_friends").hasClass("friends")) {
+        $("#topmenu_friends").removeClass("friends");
+    }
+    if (!$("#topmenu_friends").hasClass("friendsActive")) {
+        $("#topmenu_friends").addClass("friendsActive");
+    }
+}
+
+//好友选项正常显示
+function FriendNormal() {
+    if ($("#topmenu_friends").hasClass("friendsActive")) {
+        $("#topmenu_friends").removeClass("friendsActive");
+    }
+    if (!$("#topmenu_friends").hasClass("friends")) {
+        $("#topmenu_friends").addClass("friends");
+    }
+}
 
 //读取信息
 function loadChatInfo(data,right)
@@ -563,6 +742,64 @@ function loadChatInfo(data,right)
         //ChatPublishTime.text("Now");
         //ChatPublishTime.appendTo(UserInfo);
     }
+}
+
+//未读信息处理
+function UnReadMsgHandler() {
+    //若右侧面板打开，则将未读消息清空，并更新服务器数据
+    if (RightMenuState) {
+        //清空未读的数目
+        updateUnReadMsgCount(0);
+    }
+    else {
+        //当右侧面板未打开，每当有一条数据传输来就将未读信息++
+        updateUnReadMsgCount(++CurrentUnReadMsgCount);
+    }
+}
+
+//更新未读数目  同时更新服务器上的数据
+function updateUnReadMsgCount(unRead) {
+    CurrentUnReadMsgCount = unRead;
+    $("#UnReadMsg").text("当前未读消息有" + CurrentUnReadMsgCount + "条");
+    ////更新服务器上数据
+    //chat.server.UpdateServerUnReadMsg(CurrentUnReadMsgCount);
+}
+
+//给聊天面板上添加上用户发话的前置时间
+function ChatPanelAddPreTimeLabel() {
+    var myDate = new Date();
+    var _preTime = myDate.toLocaleTimeString();
+    addTimeLabel(_preTime, true);
+}
+
+//获取并添加当前时间到聊天面板上
+function addTimeLabel(time, sendToServer) {
+    //获取显示聊天信息的层
+    var $rootDiv = $("#chat-messages");
+    var timeLabel = $("<label>");
+    var timeSpan = $("<span>");
+    //当前显示的时间
+    timeSpan.text(time);
+    //将该时间显示在用户页面之中
+    timeSpan.appendTo(timeLabel);
+    timeLabel.appendTo($rootDiv);
+    //是否将当前内容传给服务器
+    if (sendToServer) {
+        //将添加的时间span内容告知服务器
+        chat.server.setPreTimeLabel(time.toString());
+    }
+}
+
+//清除当前浏览器客户端的所有聊天信息
+function ClearClientChatMsg() {
+    $("#userChatPanel #chat-messages").html("qweqwe");
+}
+
+
+//更新scrolltop
+function updateScrollTop() {
+    currentScroTop = currentScroTop + 80;
+    $("#chat-messages").scrollTop(currentScroTop)
 }
 
 
